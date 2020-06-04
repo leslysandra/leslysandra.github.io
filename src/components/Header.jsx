be8b0fc31d47f108f37a411bb85ca12cfc8b34f8
Samuel Garcia <samugv44@gmail.com>
2020-06-03 21:27:45 -0500

    Starting Landing

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../assets/styles/components/Header.css";
+
+function Header() {
+  return (
+    <header>
+      <h1>Lesly Zerna 🤖</h1>
+      <nav>
+        <ul>
+          <a href="#">Me</a>
+          <a href="#">Talks</a>
+          <a href="#">Contact</a>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
